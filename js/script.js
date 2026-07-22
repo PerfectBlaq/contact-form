a733958cc30cd684314e39181cb081875cb5c6bf
MGBECHETA TIAGO <udochukwutiago84@gmail.com>
2026-07-23 00:18:38 +0100

    Refactor form structure and enhance validation messages for better user feedback

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-document.getElementById()
+/* DOM Elements */
+const form = document.getElementsByClassName("form");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("e-mail");
+const queryType = document.getElementById('general-enquiry');
+const message = document.getElementById("message");
+const terms = document.getElementById("tc-s");
+const fieldError = document.getElementsByClassName("field__err");
+const radioError = document.getElementById("radio__err");
+const mssgError = document.getElementById("mssg__err");
+const termsError = document.getElementById("consent__err");
+console.log(firstName.value, "", " ",);
+
+form[0].addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (firstName.value === ""){
+    fieldError[0].classList.add("show");
+    firstName.style.borderColor= "red"
+  }
+  if (lastName.value === "") {
+    fieldError[1].classList.add("show");
+    lastName.style.borderColor = "red";
+  }
+  if (email.value === "" || !email.includes("@gmail.com")) {
+    fieldError[2].classList.add("show");
+    email.style.borderColor = "red";
+  }
+//   got stuck here
+  if (queryType.validity.valueMissing){
+    console.log(e);
+    radioError.className = "show";
+    
+}
+  console.log(e)
+});
