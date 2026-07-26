bccf863122cfd453d6bb06fcb72724cc332bb4e1
MGBECHETA TIAGO <udochukwutiago84@gmail.com>
2026-07-26 23:26:25 +0100

    Fix error message display and improve validation handling for form fields

--- js/script.js
+++ js/script.js
@@ -3,20 +3,20 @@ const form = document.getElementsByClassName("form");
 const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
 const email = document.getElementById("e-mail");
-const queryType = document.getElementById('general-enquiry');
+const queryType = document.getElementById("general-enquiry");
 const message = document.getElementById("message");
 const terms = document.getElementById("tc-s");
 const fieldError = document.getElementsByClassName("field__err");
 const radioError = document.getElementById("radio__err");
 const mssgError = document.getElementById("mssg__err");
 const termsError = document.getElementById("consent__err");
-console.log(firstName.value, "", " ",);
+console.log(firstName.value, "", " ");
 
 form[0].addEventListener("submit", (e) => {
   e.preventDefault();
-  if (firstName.value === ""){
+  if (firstName.value === "") {
     fieldError[0].classList.add("show");
-    firstName.style.borderColor= "red"
+    firstName.style.borderColor = "red";
   }
   if (lastName.value === "") {
     fieldError[1].classList.add("show");
@@ -26,12 +26,34 @@ form[0].addEventListener("submit", (e) => {
     fieldError[2].classList.add("show");
     email.style.borderColor = "red";
   }
-//   got stuck here
-// finally solve the error, what happened: firstly none of my radio input had a required attribut so the valuemissing was returning undefined, finally fixed it by giving one an attribute of required and secondly the class I was dynamically adding wasnt working and this is because the id has a higher specificty than the class so I had to give the styling in the class I was adding dynamically !important 
-  if (queryType.validity.valueMissing){
-
+  //   got stuck here
+  // finally solve the error, what happened: firstly none of my radio input had a required attribut so the valuemissing was returning undefined, finally fixed it by giving one an attribute of required and secondly the class I was dynamically adding wasnt working and this is because the id has a higher specificty than the class so I had to give the styling in the class I was adding dynamically !important
+  if (queryType.validity.valueMissing) {
     radioError.className = "show";
-    
-}
-  console.log(e)
+  }
+  if (message.value === "") {
+    mssgError.className = "show";
+    message.style.borderColor = "red";
+  }
+  if (!terms.checked) {
+    termsError.className = "show";
+  }
+  console.log(e);
 });
+const elements = [[firstName, lastName, email], queryType, message, terms];
+const errors = [fieldError, radioError, mssgError, termsError];
+console.log(errors[0]);
+for (let i = 0; i < 3; i++) {
+  elements[0][i].addEventListener("input", () => {
+    elements[0][i].style.borderColor = "var(--grey500-med)";
+    errors[0][i].classList.remove("show");
+  });
+}
+
+/* firstName.addEventListener("change", ()=>{
+
+})
+firstName.addEventListener("change", () => {
+
+});
+firstName.addEventListener("change", () => {}); */
