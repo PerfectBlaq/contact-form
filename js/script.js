a4751a7a26e3bb101c4b91288690d6ff92587889
MGBECHETA TIAGO <udochukwutiago84@gmail.com>
2026-07-27 03:58:31 +0100

    Refactor form validation handling and improve error display for radio inputs and message field

--- js/script.js
+++ js/script.js
@@ -4,13 +4,17 @@ const firstName = document.getElementById("first-name");
 const lastName = document.getElementById("last-name");
 const email = document.getElementById("e-mail");
 const queryType = document.getElementById("general-enquiry");
+const queryTypes = document.querySelectorAll(".radio__input");
 const message = document.getElementById("message");
 const terms = document.getElementById("tc-s");
 const fieldError = document.getElementsByClassName("field__err");
 const radioError = document.getElementById("radio__err");
 const mssgError = document.getElementById("mssg__err");
 const termsError = document.getElementById("consent__err");
-console.log(firstName.value, "", " ");
+/*DOM elements in arrays so I can access them easily using loops */
+const elements = [[firstName, lastName, email], queryTypes, message, terms];
+const errors = [fieldError, radioError, mssgError, termsError];
+// console.log(firstName.value, "", " ");
 
 form[0].addEventListener("submit", (e) => {
   e.preventDefault();
@@ -40,16 +44,33 @@ form[0].addEventListener("submit", (e) => {
   }
   console.log(e);
 });
-const elements = [[firstName, lastName, email], queryType, message, terms];
-const errors = [fieldError, radioError, mssgError, termsError];
-console.log(errors[0]);
+// console.log(errors[0]);
 for (let i = 0; i < 3; i++) {
   elements[0][i].addEventListener("input", () => {
     elements[0][i].style.borderColor = "var(--grey500-med)";
     errors[0][i].classList.remove("show");
   });
 }
-
+for (let i = 1; i <= 3; i++) {
+  if (i == 1) {
+    elements[i].forEach((query) => {
+      query.addEventListener("change", () => {
+        errors[i].classList.remove("show");
+      });
+    });
+  }
+  if (i == 2) {
+    elements[i].addEventListener("input", () => {
+      elements[i].style.borderColor = "var(--grey500-med)";
+      errors[i].classList.remove("show");
+    });
+  }
+  if (i == 3) {
+    elements[i].addEventListener("change", () => {
+      errors[i].classList.remove("show");
+    });
+  }
+}
 /* firstName.addEventListener("change", ()=>{
 
 })
